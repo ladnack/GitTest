8dbc129aa9b4918a549618ccb9e039c2db8a4f48
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-10-08 16:16:19 +0900

    Add another label

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label: UILabel = UILabel()
+        
+        label.text = "github"
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
