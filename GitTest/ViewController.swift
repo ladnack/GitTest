f758d342acc424ba256446ac1b04d9c496794bc7
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-10-08 16:56:01 +0900

    Remove "self"

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -23,8 +23,8 @@ class ViewController: UIViewController {
         let button: UIButton = UIButton()
         button.backgroundColor = UIColor.gray
         
-        self.view.addSubview(self.branch)
-        self.view.addSubview(self.orign)
+        self.view.addSubview(branch)
+        self.view.addSubview(orign)
         
         
         
