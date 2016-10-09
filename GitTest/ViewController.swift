39f865abde197a97851769da47e0f35200c3d2bc
Kyohei-Sakai <nico_f00tb@yahoo.co.jp>
2016-10-09 15:45:54 +0900

    Add alpha

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         label.text = "github"
         
         let button: UIButton = UIButton()
-        button.backgroundColor = UIColor.gray
+        button.backgroundColor = UIColor.gray.withAlphaComponent(0.8)
         
         self.view.addSubview(pull)
         
